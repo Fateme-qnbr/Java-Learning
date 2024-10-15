d4c6905f78e7a4be4cbd09d0c41f7a80a0301062
Fateme_qnbr <fatemeqanbari010@gmail.com>
2024-10-15 17:35:10 +0330

    N-Primitive-DataType

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String str = "test";
+        System.out.println("String is: "+str);
+
+        String str1 = new String("test");
+        System.out.println("Another string: "+str1);
+
+        int arr[];
+        arr = new int[4];
+        arr[0]= 0;
+        arr[1]= 1;
+        arr[2]= 2;
+        System.out.println(arr);
+        System.out.println(arr[2]);
+
     }
 }
